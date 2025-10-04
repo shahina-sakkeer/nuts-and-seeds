a18ed0d4430b65bcd82b45c6e55ddad1c94e2e6c
shahina-sakkeer <shahinabinthsakkeer@gmail.com>
2025-10-04 13:05:37 +0530

    second commit

--- admin_app/decorators.py
+++ admin_app/decorators.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import redirect
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-# def staff_required(view_func):
-#     @login_required(login_url='/custom/adminlogin/')
-#     def wrapper(request,*args,**kwargs):
-#         if request.user.is_staff:
-#             return view_func(request,*args,**kwargs)
-#         return redirect("signin")
-#     return wrapper
+def staff_required(view_func):
+    @login_required(login_url='/custom-admin/login/')
+    def wrapper(request,*args,**kwargs):
+        if request.user.is_staff:
+            return view_func(request,*args,**kwargs)
+        return redirect("signin")
+    return wrapper
--- admin_app/views.py
+++ admin_app/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from admin_app.forms import AdminLoginForms
+from admin_app.decorators import staff_required
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 # Create your views here.
 
-
+@staff_required
 def admin_dashboard(request):
     return render(request,"dashboard.html")
 
 
 def admin_login(request):
-    return render(request,"admin_login.html")
+    if request.method=="POST":
+        form=AdminLoginForms(request.POST)
+        if form.is_valid():
+            u=form.cleaned_data["username"]
+            p=form.cleaned_data["password"]
+            user=authenticate(request,username=u,password=p)
+
+            if user is not None and user.is_staff:
+                login(request,user)
+                return redirect("dashboard_home")
+            else:
+                messages.error(request,"Invalid Credentials or not and Admin!!")
+    else:
+        form=AdminLoginForms()
+
+    return render(request,"admin_login.html",{"forms":form})
